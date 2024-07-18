8a502aba93beb966912cd40ac674858dcfd198f8
Ayush <asingh2341@gmail.com>
2024-07-18 19:24:50 +0530

    SuperAdmin Registration

---
+++ Emp/Repo/AdminRepository.cs
@@ -0,0 +1,46 @@
+﻿using Emp.Controllers;
+using Emp.Models;
+using Emp.Models.Emp.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Emp.Repo
+{
+    public class AdminRepository : IAdminRepository
+    {
+        private readonly ILogger<AdminRepository> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        public AdminRepository(UserManager<IdentityUser> userManager, ILogger<AdminRepository> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public async Task<bool> Create(AdminAuthModel model)
+        {
+            _logger.LogInformation("Admin Username: {Username}", model.Username);
+
+            var user = await _userManager.FindByEmailAsync(model.Username);
+            if (user != null)
+            {
+                var result = await _userManager.CheckPasswordAsync(user, model.Password);
+
+                if (result)
+                {
+                    _logger.LogInformation("Password {Password}", model.Password);
+                    var isInRole = await _userManager.IsInRoleAsync(user, "SuperAdmin");
+                    if (isInRole)
+                    {
+                        return true;
+                    }
+                 
+                }
+            }
+            return false;
+        }
+
+    }
+}
